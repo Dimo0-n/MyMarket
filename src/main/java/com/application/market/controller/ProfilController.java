d794841e49cf0b29942d5ac2b418797aae7d7d49
Dumitras <dumitrufrimu118@gmail.com>
2024-12-26 22:47:22 +0200

    ca sa fie frumos

--- src/main/java/com/application/market/controller/ProfilController.java
+++ src/main/java/com/application/market/controller/ProfilController.java
@@ -1,13 +1,23 @@
 package com.application.market.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class ProfilController {
 
     @GetMapping("/profil")
     public String profil(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+
+
         return "profil";
     }
 
