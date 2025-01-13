085e45e288c8d92101c6fbe5b48010f7fb0e836c
Dumitras <dumitrufrimu118@gmail.com>
2025-01-13 22:36:26 +0200

    afisare mesaje pentru addToCart

--- src/main/java/com/application/market/controller/CartController.java
+++ src/main/java/com/application/market/controller/CartController.java
@@ -3,6 +3,7 @@ package com.application.market.controller;
 import com.application.market.entity.CartItems;
 import com.application.market.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +14,9 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CartController {
@@ -35,14 +38,24 @@ public class CartController {
     }
 
     @PostMapping("/addToCart/{productId}")
-    public String addToCart(@PathVariable int productId, @RequestBody CartItems request, RedirectAttributes redirectAttributes) {
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> addToCart(
+            @PathVariable int productId,
+            @RequestBody CartItems request) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
 
-        cartService.addToCart(productId, request.getQuantity(), email);
-        redirectAttributes.addFlashAttribute("message", "Product added to cart!");
-        redirectAttributes.addFlashAttribute("messageType", "success");
-        return "redirect:/cart";
+        Map<String, String> response = new HashMap<>();
+        if (email.equals("anonymousUser")) {
+            response.put("message", "Loghează-te pentru a adăuga produse în coș!");
+            response.put("messageType", "error");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        } else {
+            cartService.addToCart(productId, request.getQuantity(), email);
+            response.put("message", "Produsul a fost adăugat în coș!");
+            response.put("messageType", "success");
+            return ResponseEntity.ok(response);
+        }
     }
 
     @GetMapping("/all-products-remove")
